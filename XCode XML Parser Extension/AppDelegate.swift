4ec8298525566d320385c060a44101dbf0c4a6f6
Usama Nasar <usamanasar38@icloud.com>
2018-11-14 16:30:08 +0500

    Initial Commit

---
+++ XCode XML Parser Extension/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  XCode XML Parser Extension
+//
+//  Created by Usama Nasar on 14/11/2018.
+//  Copyright © 2018 Usama Nasar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
